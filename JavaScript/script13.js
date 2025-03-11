5f996af529fef3d9e9790d78a5f29d98fa9bfed8
TattybubuT <tattybubu.talantbekova.kk@gmail.com>
2025-03-11 07:43:01 +0600

    filterPositive, findDuplicates

---
+++ JavaScript/script13.js
@@ -0,0 +1,29 @@
+function filterPositive(arr) {
+    return arr.filter(num => num > 0);
+}
+
+const mixedNumbers = [-3, 5, -1, 7, 0, -8, 10];
+console.log(filterPositive(mixedNumbers));  
+
+function findDuplicates(arr) {
+    let duplicates = [];
+    let seen = new Set();
+    let added = new Set();
+    
+    for (let num of arr) {
+        if (seen.has(num) && !added.has(num)) {
+            duplicates.push(num);
+            added.add(num);
+        }
+        seen.add(num);
+    }
+    
+    return duplicates;
+}
+
+const data = [1, 2, 3, 4, 2, 3, 5, 6, 1];
+console.log(findDuplicates(data));  
+// [1, 2, 3]
+
+
+
